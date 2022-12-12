0739dbf279a0dc872fb690b22fcce3d64ff96266
Rishita Panchal <rishita.p@simformsolutions.com>
2022-12-12 11:03:25 +0530

    Project created

---
+++ SSCarouselCollectionView/SSCarouselCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSCarouselCollectionView
+//
+//  Created by Rishita Panchal on 02/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
